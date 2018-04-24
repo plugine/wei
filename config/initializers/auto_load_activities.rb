c9302b5c86a38cf470f23e1394d85b23287f487b
张中南 <b13040521@njupt.edu.cn>
2018-04-25 01:08:05 +0800

    fix： 自动加载

--- config/initializers/auto_load_activities.rb
+++ config/initializers/auto_load_activities.rb
@@ -1,6 +1,6 @@
 (Dir.glob "#{Rails.root}/lib/activities/**/*").each do |file|
   begin
-    require file
+    load file
   rescue Exception => e
     Rails.logger.info "error load activity #{file}, #{e.inspect}"
   end
