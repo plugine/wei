64d1c29f4ff6e2cca1ec12b59e719e398ed5c30a
张中南 <b13040521@njupt.edu.cn>
2018-05-13 05:44:01 +0800

    release: 增加payment的用户ID

--- app/services/wxpub_payment_service.rb
+++ app/services/wxpub_payment_service.rb
@@ -2,13 +2,14 @@ class WxpubPaymentService
   include Singleton
 
   def pay(pay_config_name, openid, order)
+    user = User.find_by_openid openid
     payment = Payment.generate pay_config_name: pay_config_name,
                                method: 'wxpub_payment',
                                total_fee: order.price + order.carriage,
                                order_id: order.id,
                                state: :initialized,
-                               user_id: order.user_id,
-                               openid: openid
+                               openid: openid,
+                               user_id: user.id
 
     payment.save ? payment : nil
   end
