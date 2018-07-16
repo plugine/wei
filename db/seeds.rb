5c81b7660cbf229b95dce8c4b67dbcd2fa39a87a
张中南 <zhangzhongnan@boohee.com>
2018-07-16 16:26:27 +0800

    更新lock

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,4 @@ company = Company.create name: 'haelo' unless company
 
 if CropUser.count == 0
   company.crop_users.create account: 'haelo', password: 'haelo', phone: '18351882074'
-  company.public_accounts.create(name: 'test',
-                                 account: 'gh_b3fe701588df',
-                                 appid: 'wx992037f0a32db788',
-                                 appsecret: 'ebf11ea81d4887f9fd26987241262913')
 end
